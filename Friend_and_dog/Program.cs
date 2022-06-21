a3036b99eea2b071fdc9bbec348abc487fa70eb2
Timur <arbo1234@yandex.ru>
2022-06-21 22:53:04 +0300

    Friedn and dog

---
+++ Friend_and_dog/Program.cs
@@ -0,0 +1,23 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        int time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        int time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+Console.WriteLine(count);
