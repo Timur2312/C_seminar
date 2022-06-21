fe07bae93ff7c5f8f11a1d5c2399d977fe70df5c
Timur <arbo1234@yandex.ru>
2022-06-21 22:31:08 +0300

    Ex002_Hello_User

---
+++ Ex002_Hello_User/Program.cs
@@ -0,0 +1,7 @@
+﻿// WriteLine - выводит текст и в конце делает перенос строки Console.WriteLine("Hello123, World!");
+// Console.ReadLine - выводит данные записанные в переменную 
+// Console.Write - выводит текст но не переност строку в конце
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет,");
+Console.Write(username);
